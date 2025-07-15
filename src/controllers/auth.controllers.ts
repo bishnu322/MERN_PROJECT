d0d853d813a8862ea04d7c38ab66d45ce9541f5f
bishnu_kohar <koharbishnu322@gmail.com>
2025-07-15 13:04:27 +0545

    user registration

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -1,4 +1,29 @@
+import { Request, Response } from "express";
+import { CustomError } from "../middlewares/errorHandler.middleware";
+import { User } from "../models/user.models";
+
 // registration
+export const registerUser = async (req: Request, res: Response) => {
+  try {
+    const { first_name, last_name, email, phone_number, role } = req.body;
+    const user = await User.create({
+      first_name,
+      last_name,
+      email,
+      phone_number,
+      role,
+    });
+
+    res.status(201).json({
+      message: "registration succeeded",
+      status: "success",
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    throw new CustomError("registration error", 400);
+  }
+};
 
 // login
 
