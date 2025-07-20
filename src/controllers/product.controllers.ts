cc5053875c00316abbe27487bb3a93a21f617acd
bishnu_kohar <koharbishnu322@gmail.com>
2025-07-20 16:19:59 +0545

    product controller

---
+++ src/controllers/product.controllers.ts
@@ -0,0 +1,152 @@
+import { Request, Response } from "express";
+import { asyncHandler } from "../utils/async-handler.utils";
+import { Product } from "../models/product.models";
+import { CustomError } from "../middlewares/error-handler.middleware";
+import { Brand } from "../models/brand.models";
+import Category from "../models/category.models";
+
+//* register product
+
+export const registerProduct = asyncHandler(
+  async (req: Request, res: Response) => {
+    const {
+      name,
+      brand,
+      category,
+      createdBy,
+      isFeatured,
+      stock,
+      price,
+      description,
+      size,
+    } = req.body;
+
+    if (!brand) {
+      throw new CustomError("Brand is required!", 400);
+    }
+
+    if (!category) {
+      throw new CustomError("category is required", 400);
+    }
+
+    if (!createdBy) {
+      throw new CustomError("createdBy is required", 400);
+    }
+
+    const product = new Product({
+      name,
+      isFeatured,
+      stock,
+      price,
+      description,
+      size,
+    });
+
+    const productBrand = await Brand.findById(brand);
+
+    if (!productBrand) {
+      throw new CustomError("Brand not found!", 404);
+    }
+
+    const productCategory = await Category.findById(category);
+
+    if (!productCategory) {
+      throw new CustomError("Product category not found!", 404);
+    }
+
+    product.brand = productBrand._id;
+    product.category = productCategory._id;
+    product.createdBy = createdBy;
+
+    await product.save();
+
+    res.status(201).json({
+      message: "product created successfully",
+      status: "Success",
+      success: true,
+      data: product,
+    });
+  }
+);
+
+//* get all product
+
+export const getAllProduct = asyncHandler(
+  async (req: Request, res: Response) => {
+    const product = await Product.find();
+
+    res.status(200).json({
+      message: "All product fetched",
+      status: "Success",
+      success: true,
+      data: product,
+    });
+  }
+);
+
+//* get product by id
+
+export const getProductById = asyncHandler(
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    const product = await Product.findById(id);
+
+    if (!product) {
+      throw new CustomError("Product not found!", 404);
+    }
+
+    res.status(200).json({
+      message: "product fetched by id",
+      status: "Success",
+      success: true,
+      data: product,
+    });
+  }
+);
+
+//* remove product
+
+export const removeProduct = asyncHandler(
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    const product = await Product.findByIdAndDelete(id);
+
+    if (!product) {
+      throw new CustomError("Product not found!", 404);
+    }
+
+    res.status(200).json({
+      message: "product removed successfully",
+      status: "Success",
+      success: true,
+      data: product,
+    });
+  }
+);
+
+//* update product
+
+export const updateProduct = asyncHandler(
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const payload = req.body;
+
+    const product = await Product.findByIdAndUpdate(
+      { _id: id },
+      { $set: payload }
+    );
+
+    if (!product) {
+      throw new CustomError("product not found!", 404);
+    }
+
+    res.status(200).json({
+      message: "product updated successfully",
+      status: "Success",
+      success: true,
+      data: product,
+    });
+  }
+);
