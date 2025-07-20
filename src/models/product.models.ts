167f1676ed6e623b1e9b07ae3de99d307d117a71
bishnu_kohar <koharbishnu322@gmail.com>
2025-07-20 16:19:23 +0545

    creating product model

---
+++ src/models/product.models.ts
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "product name is required"],
+      trim: true,
+    },
+    brand: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Brand",
+      required: [true, "brand is required !"],
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: [true, "Category is required !"],
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "createdBy is required !"],
+    },
+    isFeatured: {
+      type: Boolean,
+      default: false,
+    },
+    stock: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, "price is required!"],
+      min: [0, "price must be positive number"],
+    },
+    description: {
+      type: String,
+    },
+    size: {
+      type: String,
+    },
+  },
+
+  { timestamps: true }
+);
+
+export const Product = mongoose.model("Product", productSchema);
