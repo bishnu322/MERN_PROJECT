0cdfc5e9da782fb8fd0c2ae9017b8a50a1be1786
bishnu_kohar <koharbishnu322@gmail.com>
2025-08-04 21:28:40 +0545

    node mailer

--- src/utils/mailer.utils.ts
+++ src/utils/mailer.utils.ts
@@ -1,4 +1,5 @@
 import nodemailer from "nodemailer";
+import { CustomError } from "../middlewares/error-handler.middleware";
 
 export const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
@@ -9,3 +10,46 @@ export const transporter = nodemailer.createTransport({
     pass: process.env.SMTP_PASS,
   },
 });
+
+type Mailing = {
+  to: string;
+  subject: string;
+  html: string;
+  cc?: string[] | string | null;
+  bcc?: string[] | string | null;
+  attachment?: [] | null;
+};
+
+export const sendEmail = async ({
+  to,
+  subject,
+  html,
+  cc = null,
+  bcc = null,
+  attachment = null,
+}: Mailing) => {
+  try {
+    let message: Record<string, any> = {
+      from: `MERN SHOPPING CENTER <${process.env.SMTP_USER}>`,
+      to,
+      subject,
+      html,
+    };
+
+    if (cc) {
+      message["cc"] = cc;
+    }
+
+    if (bcc) {
+      message["bcc"] = bcc;
+    }
+
+    if (attachment) {
+      message["attachment"] = attachment;
+    }
+
+    await transporter.sendMail(message);
+  } catch (error) {
+    throw new CustomError(`sending email error ${error}`, 400);
+  }
+};
