f7258cbb02cd2a52401152aeb3b16f322e3b46f3
bishnu_kohar <koharbishnu322@gmail.com>
2025-08-06 12:28:14 +0545

    user pagination

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,16 +2,36 @@ import { Request, Response } from "express";
 import { User } from "../models/user.models";
 import { CustomError } from "../middlewares/error-handler.middleware";
 import { asyncHandler } from "../utils/async-handler.utils";
+import { pagination } from "../utils/pagination.utils";
 
 // get all users
 export const getAll = asyncHandler(async (req: Request, res: Response) => {
-  const user = await User.find();
+  const { current_page, per_page } = req.query;
+
+  const page = Number(current_page) | 1;
+  const limit = Number(per_page) | 10;
+  const skip = (page - 1) * limit;
+
+  const filter = {};
+  const user = await User.find(filter).limit(limit).skip(skip);
+
+  const total = await User.countDocuments(filter);
+
+  const { total_page, next_page, pre_page, has_next_page, has_pre_page } =
+    await pagination(page, limit, total);
 
   res.status(200).json({
     message: "get all data",
     status: "Success",
     success: true,
     data: user,
+    pagination: {
+      total_page,
+      next_page,
+      pre_page,
+      has_next_page,
+      has_pre_page,
+    },
   });
 });
 
