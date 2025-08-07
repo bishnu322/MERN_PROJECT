ae22ab4539f0ea41a2f146681592b9eeacde8adf
bishnu_kohar <koharbishnu322@gmail.com>
2025-08-07 13:05:09 +0545

    email changes in order controller

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -6,10 +6,12 @@ import { Order } from "../models/order.models";
 import { Order_status } from "../types/enum.types";
 import { sendEmail } from "../utils/mailer.utils";
 import { Cart } from "../models/cart.models";
+import { generate_order_status_email } from "../utils/orderEmail.utils";
 
 export const createOrder = asyncHandler(async (req: Request, res: Response) => {
   const { items, shipping_address } = req.body;
   const user = req.user._id;
+  const { email } = req.user;
 
   if (!items) {
     throw new CustomError("items required", 400);
@@ -70,67 +72,67 @@ export const createOrder = asyncHandler(async (req: Request, res: Response) => {
   await Cart.findOneAndDelete({ user });
 
   let html = `
-    <!DOCTYPE html>
-<html>
-  <body style="margin: 0; padding: 0; background-color: #f9fafb; font-family: Arial, sans-serif;">
-    <table align="center" width="700" cellpadding="0" cellspacing="0" style="background-color: #ffffff; border-radius: 8px; margin: 0 auto; padding: 20px;">
-      <!-- Header -->
-      <tr>
-        <td style="text-align: center; padding: 20px 0; font-size: 24px; font-weight: bold; color: #065f46;">
-          🛍️ Mern-kart
-        </td>
-      </tr>
+      <!DOCTYPE html>
+  <html>
+    <body style="margin: 0; padding: 0; background-color: #f9fafb; font-family: Arial, sans-serif;">
+      <table align="center" width="700" cellpadding="0" cellspacing="0" style="background-color: #ffffff; border-radius: 8px; margin: 0 auto; padding: 20px;">
+        <!-- Header -->
+        <tr>
+          <td style="text-align: center; padding: 20px 0; font-size: 24px; font-weight: bold; color: #065f46;">
+            🛍️ Mern-kart
+          </td>
+        </tr>
 
-      <!-- Message -->
-      <tr>
-        <td style="padding: 0 30px;">
-          <p style="font-size: 16px;">Hey <strong>User</strong>,</p>
-          <p style="color: #ea580c; font-size: 18px;"><span style="font-size: 20px;">✅</span> <strong>Your order is confirmed!</strong></p>
-          <p style="color: #374151;">Thanks for shopping with us! Your order hasn't shipped yet, but we’ll send you a confirmation email with tracking details as soon as it does.</p>
-        </td>
-      </tr>
+        <!-- Message -->
+        <tr>
+          <td style="padding: 0 30px;">
+            <p style="font-size: 16px;">Hey <strong>User</strong>,</p>
+            <p style="color: #ea580c; font-size: 18px;"><span style="font-size: 20px;">✅</span> <strong>Your order is confirmed!</strong></p>
+            <p style="color: #374151;">Thanks for shopping with us! Your order hasn't shipped yet, but we’ll send you a confirmation email with tracking details as soon as it does.</p>
+          </td>
+        </tr>
 
-      <!-- Spacer -->
-      <tr><td style="height: 30px;"></td></tr>
+        <!-- Spacer -->
+        <tr><td style="height: 30px;"></td></tr>
 
-      <!-- Table -->
-      <tr>
-        <td style="padding: 0 30px;">
-          <table width="100%" cellpadding="0" cellspacing="0" style="border-collapse: collapse; background-color: #f1f5f9; border-radius: 8px; overflow: hidden;">
-            <thead style="background-color: #cbd5e1;">
-              <tr>
-                <th style="padding: 12px; text-align: left; font-weight: bold;">Product</th>
-                <th style="padding: 12px; text-align: left; font-weight: bold;">Quantity</th>
-                <th style="padding: 12px; text-align: left; font-weight: bold;">Price</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr style="background-color: #ffffff;">
-                <td style="padding: 12px; color: #6b21a8;">${newOrder.items[0].product}</td>
-                <td style="padding: 12px;">${newOrder.items[0].quantity}</td>
-                <td style="padding: 12px; color: #6b21a8;">${newOrder.items[0].total_price}</td>
-              </tr>
-              <tr style="background-color: #f3f4f6;">
-                <td colspan="2" style="padding: 12px; text-align: right; font-weight: bold; color: #6b21a8;">Total Amount</td>
-                <td style="padding: 12px; font-weight: bold; color: #6b21a8;">${newOrder.total_amount}</td>
-              </tr>
-            </tbody>
-          </table>
-        </td>
-      </tr>
+        <!-- Table -->
+        <tr>
+          <td style="padding: 0 30px;">
+            <table width="100%" cellpadding="0" cellspacing="0" style="border-collapse: collapse; background-color: #f1f5f9; border-radius: 8px; overflow: hidden;">
+              <thead style="background-color: #cbd5e1;">
+                <tr>
+                  <th style="padding: 12px; text-align: left; font-weight: bold;">Product</th>
+                  <th style="padding: 12px; text-align: left; font-weight: bold;">Quantity</th>
+                  <th style="padding: 12px; text-align: left; font-weight: bold;">Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr style="background-color: #ffffff;">
+                  <td style="padding: 12px; color: #6b21a8;">${newOrder.items[0].product}</td>
+                  <td style="padding: 12px;">${newOrder.items[0].quantity}</td>
+                  <td style="padding: 12px; color: #6b21a8;">${newOrder.items[0].total_price}</td>
+                </tr>
+                <tr style="background-color: #f3f4f6;">
+                  <td colspan="2" style="padding: 12px; text-align: right; font-weight: bold; color: #6b21a8;">Total Amount</td>
+                  <td style="padding: 12px; font-weight: bold; color: #6b21a8;">${newOrder.total_amount}</td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
 
-      <!-- Footer Spacer -->
-      <tr><td style="height: 30px;"></td></tr>
-    </table>
-  </body>
-</html>
+        <!-- Footer Spacer -->
+        <tr><td style="height: 30px;"></td></tr>
+      </table>
+    </body>
+  </html>
 
-  `;
+    `;
 
   await sendEmail({
     html: html,
     subject: "Order Confirmed",
-    to: "gautamkohar110@gmail.com",
+    to: email,
   });
 
   res.status(201).json({
--- src/utils/orderEmail.utils.ts
+++ src/utils/orderEmail.utils.ts
@@ -1,1 +1,3 @@
-export const generate_order_status_email = () => {};
+import { sendEmail } from "./mailer.utils";
+
+export const generate_order_status_email = (order: [], email: string) => {};
